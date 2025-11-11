a340dadd7ba86dc4fef6f457dba4e425b9191afc
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-11-11 10:58:45 +0530

    minor improvements

--- app/page.tsx
+++ app/page.tsx
@@ -4,45 +4,22 @@ import { callActor } from "@/machines/callMachine";
 
 export default function Home() {
   const state = useSelector(callActor, (snapshot) => snapshot.value);
-  console.log(state, "page");
   return (
-    <div className="flex h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
+    <div className="flex grow items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex grow w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <button
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            disabled={state === "idle"}
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] disabled:opacity-50"
             onClick={() => callActor.send({ type: 'DISCONNECT' })}
           >
 
-            Disconnect from Call
+            Disconnect
           </button>
           <button
             disabled={state === "on_call"}
             onClick={() => callActor.send({ type: 'CONNECT', call_id: '123' })}
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/08 px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/08 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px] disabled:opacity-50"
           >
             Connect to Call
           </button>
--- components/CallView.tsx
+++ components/CallView.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useSelector } from '@xstate/react';
 import { callActor } from '@/machines/callMachine';
+import { Drawer } from 'vaul';
 
 const CallView = () => {
     const state = useSelector(callActor, (snapshot) => snapshot.value);
-    // console.log(state, "CallView");
+
     if (state === "idle") return null;
-    return <div>CallView</div>;
+    return <div className='bg-zinc-800'>CallView</div>;
 };
 
 export default CallView;
