8721e363a557629c58302e41a1a0099142460571
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-11-11 11:35:50 +0530

    drawer integration

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,13 @@ export default function Home() {
     <div className="flex grow items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex grow w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <button
-            disabled={state === "idle"}
+          {state === "on_call" && <button
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] disabled:opacity-50"
             onClick={() => callActor.send({ type: 'DISCONNECT' })}
           >
 
             Disconnect
-          </button>
+          </button>}
           <button
             disabled={state === "on_call"}
             onClick={() => callActor.send({ type: 'CONNECT', call_id: '123' })}
--- components/CallView.tsx
+++ components/CallView.tsx
@@ -3,11 +3,45 @@ import { useSelector } from '@xstate/react';
 import { callActor } from '@/machines/callMachine';
 import { Drawer } from 'vaul';
 
+type CallParticipants = {
+    name: string;
+    id: string;
+    status: 'online' | 'offline' | 'busy';
+}
+
+type Call = {
+    callId: string;
+    participants: CallParticipants[];
+}
+
 const CallView = () => {
     const state = useSelector(callActor, (snapshot) => snapshot.value);
+    const showCallView = state === "on_call";
 
-    if (state === "idle") return null;
-    return <div className='bg-zinc-800'>CallView</div>;
+    return <Drawer.Root open={showCallView} defaultOpen={showCallView} dismissible={false}>
+        <Drawer.Portal>
+            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+            <Drawer.Content className="bg-black flex flex-col mt-24 h-screen fixed bottom-0 left-0 right-0 outline-none">
+                <div className="p-4 bg-neutral-950 flex-1">
+                    <div className="max-w-md mx-auto">
+                        <Drawer.Title className="font-medium mb-4 text-white hidden">Call View</Drawer.Title>
+
+                    </div>
+                    <button className="rounded-md mt-4 w-full bg-foreground px-3.5 py-2.5 text-sm font-semibold text-background shadow-sm hover:bg-foreground/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600" onClick={() => callActor.send({ type: 'DISCONNECT' })}>Disconnect</button>
+                </div>
+            </Drawer.Content>
+        </Drawer.Portal>
+    </Drawer.Root>;
 };
+
+const dummyCall: Call = {
+    callId: '123',
+    participants: [
+        { name: 'John Doe', id: '1', status: 'online' },
+        { name: 'Jane Doe', id: '2', status: 'offline' },
+        { name: 'Jim Doe', id: '3', status: 'busy' },
+    ],
+}
+
 
 export default CallView;
