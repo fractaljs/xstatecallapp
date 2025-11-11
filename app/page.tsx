d926c9563949415f7fdc05fde61aa7f5e413380a
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-11-11 19:11:51 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
       <main className="flex grow w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           {state === "on_call" && <button
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] disabled:opacity-50"
+
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/08 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px] disabled:opacity-50"
             onClick={() => callActor.send({ type: 'DISCONNECT' })}
           >
 
@@ -18,7 +19,7 @@ export default function Home() {
           <button
             disabled={state === "on_call"}
             onClick={() => callActor.send({ type: 'CONNECT' })}
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/08 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px] disabled:opacity-50"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] disabled:opacity-50"
           >
             Connect to Call
           </button>
--- components/CallView.tsx
+++ components/CallView.tsx
@@ -24,40 +24,35 @@ const CallView = () => {
         <Drawer.Portal>
             <Drawer.Overlay className="fixed inset-0 bg-black/40" />
             <Drawer.Content className="bg-[#040303] flex flex-col mt-24 h-screen fixed bottom-0 left-0 right-0 outline-none">
-                <div className="p-4 bg-[#040303] flex-1">
-                    <div className="max-w-md mx-auto">
+                <div className="p-4 bg-[#181414] flex-1">
+                    <div className="max-w-md mx-auto h-full flex flex-col justify-between items-center gap-4">
                         <Drawer.Title className="font-medium mb-4 text-white hidden">Call View</Drawer.Title>
 
                         <CallHeader />
+                        <button className="rounded-md mt-4 w-full bg-foreground px-3.5 py-2.5 text-sm font-semibold text-background shadow-sm hover:bg-foreground/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600" onClick={() => callActor.send({ type: 'DISCONNECT' })}>Disconnect</button>
                     </div>
-                    <button className="rounded-md mt-4 w-full bg-foreground px-3.5 py-2.5 text-sm font-semibold text-background shadow-sm hover:bg-foreground/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600" onClick={() => callActor.send({ type: 'DISCONNECT' })}>Disconnect</button>
                 </div>
             </Drawer.Content>
         </Drawer.Portal>
     </Drawer.Root>;
 };
 
-const dummyCall: CallType = {
-    callId: '123',
-    participants: [
-        { name: 'John Doe', id: '1', status: 'online' },
-        { name: 'Jane Doe', id: '2', status: 'offline' },
-        { name: 'Jim Doe', id: '3', status: 'busy' },
-    ],
-}
-
 const CallHeader = () => {
     const state = useSelector(callActor, (snapshot) => snapshot.value);
+
+    if (state === "disconnected" || state === "failed_to_connect") {
+        return null;
+    }
     const callStatus = state === "calling" ? "Calling" : "Ringing";
-    return <div className='flex items-center justify-between gap-2'>
-        <button className='size-10 bg-[#191818] rounded-full flex items-center justify-center'>
+    return <div className='flex items-center justify-between gap-2 w-full'>
+        <button disabled={true} className='size-10 bg-[#2c2525] rounded-full flex items-center justify-center outline-1 outline-neutral-700 active:scale-[0.95]'>
             <Minimize2 className='text-white rotate-90' size={14} />
         </button>
         <div className='flex flex-col items-center justify-center text-center'>
             <small className='text-white text-sm font-medium'>John Doe</small>
             <small className='text-gray-50 text-xs font-medium'>{callStatus}</small>
         </div>
-        <button className="size-10 bg-[#191818] rounded-full flex items-center justify-center">
+        <button className="size-10 bg-[#2c2525] rounded-full flex items-center justify-center outline-1 outline-neutral-700 active:scale-[0.95]">
             <Users2Icon className="text-white" size={14} />
         </button>
     </div>
